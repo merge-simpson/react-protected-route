e7fcb5d95dbcb0d9886e944be70b582234850064
merge-simpson <letsdev@kakao.com>
2022-10-13 04:40:57 +0900

    feat(route): surround <App /> with browser router

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <Routes>
+      <App />
+    </Routes>
+  </BrowserRouter>
+);
