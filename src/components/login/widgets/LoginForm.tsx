1a45a3e02ff44bde31783ab7b76ff1bf44a0107f
merge-simpson <letsdev@kakao.com>
2022-10-13 17:52:20 +0900

    feat(login): create non submit button

--- src/components/login/widgets/LoginForm.tsx
+++ src/components/login/widgets/LoginForm.tsx
@@ -16,15 +16,26 @@ const LoginForm: FC<LoginFormProps> = (props) => {
       <input value="abc1234" onChange={() => {}} />
       <input type="password" value="q1w2e3!@" onChange={() => {}} />
       <button>SEND OTP</button>
-      <button
-        onClick={(evt) => {
-          evt.preventDefault();
-        }}
-      >
-        CANCEL
-      </button>
+      <NonSubmitButton>CANCEL</NonSubmitButton>
     </form>
   );
+};
+
+interface NonSubmitButtonProps
+  extends React.DetailedHTMLProps<
+    React.ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  > {}
+
+const NonSubmitButton: FC<NonSubmitButtonProps> = (props) => {
+  const { onClick: forwardClickEventHandler, ...restProps } = props;
+
+  const onClick: React.MouseEventHandler<HTMLButtonElement> = (evt) => {
+    evt.preventDefault();
+    !!forwardClickEventHandler && forwardClickEventHandler(evt);
+  };
+
+  return <button {...restProps} onClick={onClick} />;
 };
 
 export default LoginForm;
